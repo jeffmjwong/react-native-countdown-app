015d290bb88022ea1d31a74d644cf7be70b7bf12
Jeff Wong <jeffmjwong@gmail.com>
2019-07-03 20:08:00 +1000

    Create render method

--- EventList.js
+++ EventList.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import { Text } from 'react-native';
 
 class EventList extends Component {
-
+  render() {
+    return (
+      <Text>Hello!</Text>
+    )
+  }
 }
 
 export default EventList;
