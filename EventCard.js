01812e1b44fb3daf85c98be708a3688294809f62
Jeff Wong <jeffmjwong@gmail.com>
2019-07-04 19:49:20 +1000

    Render counter stuff

--- EventCard.js
+++ EventCard.js
@@ -14,6 +14,28 @@ export default function EventCard({ event }) {
         <Text style={styles.date}>{formatDate(event.date)}</Text>
         <Text style={styles.title}>{event.title}</Text>
       </View>
+
+      <View style={styles.counterContainer}>
+        <View style={styles.counter}>
+          <Text style={styles.counterText}>{days}</Text>
+          <Text style={styles.counterLabel}>DAYS</Text>
+        </View>
+
+        <View style={styles.counter}>
+          <Text style={styles.counterText}>{hours}</Text>
+          <Text style={styles.counterLabel}>HOURS</Text>
+        </View>
+
+        <View style={styles.counter}>
+          <Text style={styles.counterText}>{minutes}</Text>
+          <Text style={styles.counterLabel}>MINUTES</Text>
+        </View>
+
+        <View style={styles.counter}>
+          <Text style={styles.counterText}>{seconds}</Text>
+          <Text style={styles.counterLabel}>SECONDS</Text>
+        </View>
+      </View>
     </View>
   );
 }
