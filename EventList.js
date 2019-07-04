927c730ecc0148ea287f6dd7583a1536bf77aa35
Jeff Wong <jeffmjwong@gmail.com>
2019-07-04 20:47:52 +1000

    Add style and countdown logic in main EventList component

--- EventList.js
+++ EventList.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
-import { FlatList, Text } from 'react-native';
+import { FlatList, Text, StyleSheet } from 'react-native';
 
 import EventCard from './EventCard';
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+    paddingTop: 40,
+    backgroundColor: '#f3f3f3'
+  }
+})
 
 class EventList extends Component {
   state = {
@@ -9,6 +17,15 @@ class EventList extends Component {
   };
 
   componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        events: this.state.events.map(evt => ({
+          ...evt,
+          timer: Date.now()
+        }))
+      });
+    }, 500);
+
     const events = require('./db.json').events.map(e => ({
       ...e,
       date: new Date(e.date)
@@ -19,6 +36,7 @@ class EventList extends Component {
   render() {
     return (
       <FlatList
+        style={styles.list}
         data={this.state.events}
         renderItem={({ item }) => <EventCard event={item} />}
         keyExtractor={item => item.id}
