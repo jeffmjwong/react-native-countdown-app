eef617472256e7ce93cb5f21e5f4a49b4b13b8c6
Jeff Wong <jeffmjwong@gmail.com>
2019-07-04 19:37:45 +1000

    Add prop types and styles component

--- EventCard.js
+++ EventCard.js
@@ -2,3 +2,18 @@ import React from 'react';
 import { Text, View, Stylesheet } from 'react-native';
 import PropTypes from 'prop-types';
 import { formatDate, getCountdownParts } from './api';
+
+const styles = Stylesheet.create({
+
+});
+
+export default function EventCard({ event }) {
+
+}
+
+EventCard.propTypes = {
+  event: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    date: PropTypes.instanceOf(Date)
+  })
+};
