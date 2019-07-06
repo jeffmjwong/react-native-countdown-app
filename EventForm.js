ae71f0d52f662616fc95408686cf3168d29a56ad
Jeff Wong <jeffmjwong@gmail.com>
2019-07-06 13:16:35 +1000

    Add main content to EventForm component

--- EventForm.js
+++ EventForm.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react';
 import { View, Text, TouchableHighlight } from 'react-native';
 
 class EventForm extends Component {
+  handleAddPress = () => {
+    // ?
+  }
+
   render() {
     return (
-
+      <View>
+        <TouchableHighlight
+          onPress={this.handleAddPress}
+        >
+          <Text>Add</Text>
+        </TouchableHighlight>
+      </View>
     );
   }
 }
