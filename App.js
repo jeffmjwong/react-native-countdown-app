88a1bf48affb5aa1fcbecc919b99ae66f9b034c2
Jeff Wong <jeffmjwong@gmail.com>
2019-07-06 14:02:02 +1000

    Add navigation for EventList and EventForm

--- App.js
+++ App.js
@@ -2,10 +2,20 @@ import React from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 import EventList from './EventList';
+import EventForm from './EventForm';
 
 const AppNavigator = createStackNavigator({
   list: {
-    screen: EventList
+    screen: EventList,
+    navigationOptions: () => ({
+      title: 'Your Events'
+    })
+  },
+  form: {
+    screen: EventForm,
+    navigationOptions: () => ({
+      title: 'Add an event'
+    })
   }
 })
 
