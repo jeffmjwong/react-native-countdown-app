29b6a96485df1e640a63abc76394e647c30a3154
Jeff Wong <jeffmjwong@gmail.com>
2019-07-06 12:01:30 +1000

    Create EventFrom component

---
+++ EventForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text, TouchableHighlight } from 'react-native';
+
+class EventForm extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default EventForm;
